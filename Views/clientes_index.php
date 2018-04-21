a95c9306e1886ccc06ff82825022b073e3a6d91a
Douglas <Douglasantana007@gmail.com>
2018-04-20 22:16:46 -0300

    [ ADD ] Início de conexão com o DB
    Inserido DB na tela de listagem de clintes

--- Views/clientes_index.php
+++ Views/clientes_index.php
@@ -6,7 +6,22 @@
  * Time: 08:07
  */
 include('../Include/Navbar.php');
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "pi";
+
+$conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
+
+if (mysqli_connect_errno()) {
+    die("Conexão falhou :" . mysqli_connect_errno());
+}
+$consulta_clientes = "SELECT * FROM pi.clientes";
+
+// faz a consulta sql, usando o parametro da conexão de banco e a query para trazer os dados.
+$clientes = mysqli_query($conexao, $consulta_clientes);
 ?>
+
     <title>Clientes</title>
     <!--<!-- Aqui entra o conteúdo da página-->
 
@@ -18,8 +33,8 @@ include('../Include/Navbar.php');
                         <div>
                             <h4 class="grey-text" align="center">Quadro de Clientes</h4>
                         </div>
-                        <!-- Aqui entra um foreach-->
                         <table class="striped bordered responsive-table">
+                            <?php// foreach ($clientes as $dado) { ?>
                             <thead>
                             <tr>
                                 <th>Ação</th>
@@ -42,37 +57,14 @@ include('../Include/Navbar.php');
                                 <td>(xx) x xxxx-xxxx</td>
                                 <td>(xx) x xxxx-xxxx</td>
                             </tr>
-                            <tr>
-                                <td>
-                                    <a class="waves-effect waves-light" href="#"><i
-                                                class="material-icons left">mode_edit</i></a>
-                                    <a href="#"><i
-                                                class="material-icons left red-text">delete</i></a>
-                                </td>
-                                <td>xxxx</td>
-                                <td>xxxxx xx xxxx</td>
-                                <td>(xx) x xxxx-xxxx</td>
-                                <td>(xx) x xxxx-xxxx</td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <a class="waves-effect waves-light" href="#"><i
-                                                class="material-icons left">mode_edit</i></a>
-                                    <a href="#"><i
-                                                class="material-icons left red-text">delete</i></a>
-                                </td>
-                                <td>xxxx</td>
-                                <td>xxxxx xx xxxx</td>
-                                <td>(xx) x xxxx-xxxx</td>
-                                <td>(xx) x xxxx-xxxx</td>
-                            </tr>
+                            <?php// } ?>
                             </tbody>
                         </table>
                     </div>
                     <div class="right-align">
-                        <a class="btn-floating btn-large waves-effect waves-light"
-                           href="#"><i
-                                    class="material-icons">add</i></a>
+                        <a class="btn-floating btn-large waves-effect waves-light" href="#">
+                            <i class="material-icons">add</i>
+                        </a>
                     </div>
                 </div>
             </div>
@@ -83,4 +75,6 @@ include('../Include/Navbar.php');
 // depois resolver o bug deste include
 include('../Include/footer.php');
 
+// encerra a conexão com o banco de dados
+mysqli_close($conexao);
 ?>
